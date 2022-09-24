1ca67a046a63718f488086b72c18c5846031f50f
brittanymcgarr <britmcgarr@gmail.com>
2022-09-24 16:43:10 -0700

    PUSH-0009 room messages

--- app/controllers/room_messages_controller.rb
+++ app/controllers/room_messages_controller.rb
@@ -1,2 +1,17 @@
 class RoomMessagesController < ApplicationController
+  before_action :load_entities
+
+  # Create a new message with the current, set user
+  def create
+    @room_message = RoomMessage.create user: current_user,
+                                       room: @room,
+                                       message: params.dig(:room_message, :message)
+  end
+
+  protected
+
+  # Load the room messages
+  def load_entities
+    @room = Room.find params.dig(:room_message, :room_id)
+  end
 end
